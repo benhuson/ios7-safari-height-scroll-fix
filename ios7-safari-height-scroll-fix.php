27472efc57ff23ceec17785cac78d09174c78b58
Ben Huson <ben@thewhiteroom.net>
2014-01-28 10:55:52 +0000

    Add GitHub URI.

--- ios7-safari-height-scroll-fix.php
+++ ios7-safari-height-scroll-fix.php
@@ -2,9 +2,10 @@
 
 /*
 Plugin Name: iOS7 Safari Height Scroll Fix
+Plugin URI: https://github.com/benhuson/ios7-safari-height-scroll-fix
 Description: When setting body height to 100% in Safari iOS7 the page still scrolls around 20px. <a href="http://stackoverflow.com/questions/19012135/ios-7-ipad-safari-landscape-innerheight-outerheight-layout-issue" target="IOS7_Safari_Height_Scroll_Fix">This fix</a> prevents that. Use the 'ios7_safari_height_scroll_fix_required' filter to disable the fix on certain pages.
 Version: 1.0
-Author: Camber
+Author: Ben Huson
 */
 
 class IOS7_Safari_Height_Scroll_Fix {
